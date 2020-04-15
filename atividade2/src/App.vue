bb9d43a049bbfefc810f7adc6ebec96b1bde7c5b
dragonxzy <dragonxzy100@gmail.com>
2020-04-15 00:25:45 -0300

    !

---
+++ atividade2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <TodoForm :tarefas="tarefas" @AdicionarTexto="Adicionar($event)" />
+    <TodoList :tarefas="tarefas"/>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+    TodoForm
+  },
+  data: function() {
+    return {
+      tarefas: []};
+  },
+  methods: {
+    Adicionar: function(event) {
+      this.tarefas.push({
+        texto: event.titulo,
+        cor: event.cor
+      })
+      }
+    }
+}
+</script>
+<style>
+</style>
---
+++ atividade2/src/components/TodoForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="adicionar">
+    <div class="form">
+      <input type="text" placeholder="Adicione uma Tarefa" v-model="titulo"/>
+      <button @click="adicionar">Adicionar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoForm",
+  props: ["tarefas"],
+  data: function() {
+    return {
+      titulo: "",
+      cor: "background: green"
+    };
+  },
+  methods: {
+    adicionar: function() {
+      this.$emit("AdicionarTexto", {
+        titulo: this.titulo,
+        cor: this.cor
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#adicionar {
+  width: 435px;
+  height: 50px;
+  border: 1px solid #fff;
+  margin: 30px auto 0 auto;
+}
+.form input {
+  margin: 8px 0 10px 10px;
+  user-select: none;
+  border: none;
+  width: 300px;
+  height: 30px;
+  font-size: 17px;
+}
+.form button {
+  margin-left: 10px;
+  border: none;
+  border-radius: 2px;
+  width: 100px;
+  height: 30px;
+  cursor: pointer;
+}
+</style>
